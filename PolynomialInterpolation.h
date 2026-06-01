0ea6b8c80748452b4cff9bd7517af18c9944d85d
Yoon-Ki Hong <mymy49@nate.com>
2026-06-01 22:30:06 +0900

    Move existing code

---
+++ PolynomialInterpolation.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Yoon-Ki Hong
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file "LICENSE" in the main directory of this archive for more details.
+ */
+
+#ifndef YSS_POLYNOMIAL_INTERPOLATION__H_
+#define YSS_POLYNOMIAL_INTERPOLATION__H_
+
+#include <stdint.h>
+
+class PolynomialInterpolation
+{
+public:
+	PolynomialInterpolation(uint16_t maxPoint);
+
+	void setPoint(uint16_t num, float value, float adc);
+
+	void setNumberOfPoint(uint16_t num);
+
+	void getPoint(uint16_t num, float *value, float *adc);
+
+	uint16_t getNumberOfPoint(void);
+
+	float calculate(float adc);
+
+private :
+	uint16_t mMaxPoint, mWorkingPoint;
+	float *mValue, *mAdc;
+};
+
+#endif
